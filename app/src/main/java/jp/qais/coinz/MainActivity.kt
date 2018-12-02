1475a3727c8bc984138d9186913a64191d0fc52d
Qais Patankar <qaisjp@gmail.com>
2018-12-02 01:08:15 +0000

    add basic preferences

--- app/src/main/java/jp/qais/coinz/MainActivity.kt
+++ app/src/main/java/jp/qais/coinz/MainActivity.kt
@@ -1,5 +1,6 @@
 package jp.qais.coinz
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -14,6 +15,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+    private var downloadDate = "" // Format: YYYY/MM/DD
+    private var darkMode = false
+    private val preferencesFile = "MyPrefsFile"
+
 
     private var tally = 0
 
@@ -40,13 +47,42 @@ class MainActivity : AppCompatActivity() {
         task.execute(URL("http://homepages.inf.ed.ac.uk/stg/coinz/2018/12/01/coinzmap.geojson"))
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        // Restore preferences
+        val settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE)
+
+        // Initialise default values
+        downloadDate = settings.getString("lastDownloadDate", "")
+        darkMode = settings.getBoolean("darkMode", darkMode)
+
+        Log.d(tag, "[onStart] lastDownloadDate is $downloadDate")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(tag, "[onStop] Storing preferences")
+
+        val settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE)
+        val editor = settings.edit()
+        editor.putString("lastDownloadDate", downloadDate)
+        editor.putBoolean("darkMode", darkMode)
+
+        // Apply edits
+        editor.apply()
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
 
         val switchDarkMode = menu.findItem(R.id.action_darkmode).actionView.findViewById<Switch>(R.id.darkModeSwitch)
+        switchDarkMode.isChecked = darkMode
         switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
-            Log.d("darkMode", if (isChecked) "YEAH BOIS" else "-.-")
+            darkMode = isChecked
+            Log.d("darkMode", if (darkMode) "YEAH BOIS" else "-.-")
         }
         return true
     }
