6f063ba935b4fb9c26110845de898cc078579442
Qais Patankar <qaisjp@gmail.com>
2018-12-05 22:51:53 +0000

    Add rudimentary play support

--- app/src/main/java/jp/qais/coinz/GameActivity.kt
+++ app/src/main/java/jp/qais/coinz/GameActivity.kt
@@ -2,7 +2,9 @@ package jp.qais.coinz
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.mapbox.mapboxsdk.maps.MapFragment
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
@@ -10,25 +12,31 @@ class GameActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_play)
+                val fragment = MapFragment()
+                fragmentManager.beginTransaction().let {
+                    it.replace(R.id.gameFrame, fragment as android.app.Fragment)
+//                    it.addToBackStack(null) // this pushes to stack
+                    it.commit()
+                }
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_leaderboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_account -> {
-                message.setText(R.string.title_account)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_payments -> {
-                message.setText(R.string.title_payments)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+//            R.id.navigation_dashboard -> {
+//                message.setText(R.string.title_leaderboard)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_account -> {
+//                message.setText(R.string.title_account)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_payments -> {
+//                message.setText(R.string.title_payments)
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.navigation_notifications -> {
+//                message.setText(R.string.title_notifications)
+//                return@OnNavigationItemSelectedListener true
+//            }
         }
         false
     }
