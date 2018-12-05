8766ff6bf06abc4f25e536edfdae0e2a42853360
Qais Patankar <qaisjp@gmail.com>
2018-12-05 23:50:18 +0000

    game: use custom toolbar
    BottomNavigationView will behave oddly (visually) when you hide the
    original ActionBar. Using a custom Toolbar solves this issue.

--- app/src/main/java/jp/qais/coinz/GameActivity.kt
+++ app/src/main/java/jp/qais/coinz/GameActivity.kt
@@ -41,6 +41,8 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        setSupportActionBar(toolbar)
+
 
         BottomNavigationViewHelper.removeShiftMode(navigation)
 
