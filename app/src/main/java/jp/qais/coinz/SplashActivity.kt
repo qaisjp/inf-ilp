b751df90dea97ee0f3414b9ef30e4f11ba84b3c1
Qais Patankar <qaisjp@gmail.com>
2018-12-05 18:43:12 +0000

    splash: remove fab

--- app/src/main/java/jp/qais/coinz/SplashActivity.kt
+++ app/src/main/java/jp/qais/coinz/SplashActivity.kt
@@ -39,12 +39,6 @@ class SplashActivity : AppCompatActivity() {
 
         // Set up the ViewPager with the sections adapter.
         container.adapter = mSectionsPagerAdapter
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
     }
 
 
