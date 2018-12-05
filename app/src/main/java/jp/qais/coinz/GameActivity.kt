bcee1b397ccaa9d8b38de989f033470abd934877
Qais Patankar <qaisjp@gmail.com>
2018-12-05 23:02:22 +0000

    Use labelVisibilityMode compat from SDK v28

--- app/src/main/java/jp/qais/coinz/GameActivity.kt
+++ app/src/main/java/jp/qais/coinz/GameActivity.kt
@@ -1,11 +1,18 @@
 package jp.qais.coinz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.mapbox.mapboxsdk.maps.MapFragment
 import kotlinx.android.synthetic.main.activity_game.*
+import android.support.design.internal.BottomNavigationItemView
+import java.lang.reflect.AccessibleObject.setAccessible
+import java.lang.reflect.Array.setBoolean
+import android.support.design.internal.BottomNavigationMenuView
+import timber.log.Timber
+
 
 class GameActivity : AppCompatActivity() {
 
@@ -45,6 +52,33 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+        removeShiftMode(navigation)
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    // From https://stackoverflow.com/a/47407229/1517394
+    @SuppressLint("RestrictedApi")
+    private fun removeShiftMode(view: BottomNavigationView) {
+        //this will remove shift mode for bottom navigation view
+        val menuView = view.getChildAt(0) as BottomNavigationMenuView
+        try {
+            val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+            shiftingMode.isAccessible = true
+            shiftingMode.setBoolean(menuView, false)
+            shiftingMode.isAccessible = false
+            for (i in 0 until menuView.childCount) {
+                val item = menuView.getChildAt(i) as BottomNavigationItemView
+                item.setShiftingMode(false)
+                // set once again checked value, so view will be updated
+                item.setChecked(item.itemData.isChecked)
+            }
+
+        } catch (e: NoSuchFieldException) {
+            Timber.e("ERROR NO SUCH FIELD: Unable to get shift mode field")
+        } catch (e: IllegalAccessException) {
+            Timber.e("ERROR ILLEGAL ALG: Unable to change value of shift mode")
+        }
+
     }
 }
