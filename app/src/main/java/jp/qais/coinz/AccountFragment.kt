714a07afa16b312e04a7be77d07f45134be4db96
Qais Patankar <qaisjp@gmail.com>
2018-12-06 02:42:07 +0000

    Add click events for toolbar actions

--- app/src/main/java/jp/qais/coinz/AccountFragment.kt
+++ app/src/main/java/jp/qais/coinz/AccountFragment.kt
@@ -3,8 +3,10 @@ package jp.qais.coinz
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass.
@@ -12,11 +14,23 @@ import android.view.ViewGroup
  */
 class AccountFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId != R.id.action_settings) {
+            return false
+        }
 
+        Toast.makeText(requireContext(), "Settings", Toast.LENGTH_SHORT).show()
+        return true
+    }
 }
--- app/src/main/java/jp/qais/coinz/PaymentsFragment.kt
+++ app/src/main/java/jp/qais/coinz/PaymentsFragment.kt
@@ -3,9 +3,9 @@ package jp.qais.coinz
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
+import timber.log.Timber
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,11 +19,23 @@ private const val ARG_PARAM2 = "param2"
  */
 class PaymentsFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_payments, container, false)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId != R.id.action_search) {
+            return false
+        }
 
+        Toast.makeText(requireContext(), "Search", Toast.LENGTH_SHORT).show()
+        return true
+    }
 }
