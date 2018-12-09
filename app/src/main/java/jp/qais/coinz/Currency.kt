fb478934ad4d04883d98d8992d98b3057e7ca60e
Qais Patankar <qaisjp@gmail.com>
2018-12-09 13:18:42 +0000

    Give Currency colors

--- app/src/main/java/jp/qais/coinz/Currency.kt
+++ app/src/main/java/jp/qais/coinz/Currency.kt
@@ -2,12 +2,13 @@ package jp.qais.coinz
 
 import android.content.Context
 
-enum class Currency(val textID: Int) {
-    GOLD(R.string.currency_gold),
-    PENY(R.string.currency_peny),
-    DOLR(R.string.currency_dolr),
-    SHIL(R.string.currency_shil),
-    QUID(R.string.currency_quid);
+enum class Currency(val textID: Int, val colorID: Int) {
+    GOLD(R.string.currency_gold, R.color.currency_gold),
+    PENY(R.string.currency_peny, R.color.currency_peny),
+    DOLR(R.string.currency_dolr, R.color.currency_dolr),
+    SHIL(R.string.currency_shil, R.color.currency_shil),
+    QUID(R.string.currency_quid, R.color.currency_quid);
 
-    fun getString(ctx: Context): String = ctx.getString(textID)
+    fun getString(ctx: Context) = ctx.getString(textID)
+    fun getColor(ctx: Context) = ctx.getColor(colorID)
 }
