bf439ad441ba31d1ee52d18ab446994372e3a344
Qais Patankar <qaisjp@gmail.com>
2018-12-09 13:18:31 +0000

    Expand Coin constructor

--- app/src/main/java/jp/qais/coinz/Coin.kt
+++ app/src/main/java/jp/qais/coinz/Coin.kt
@@ -2,6 +2,5 @@ package jp.qais.coinz
 
 import com.mapbox.mapboxsdk.geometry.LatLng
 
-class Coin(val latLng: LatLng) {
-
+class Coin(val id: String, val currency: Currency, val latLng: LatLng, val value: Float) {
 }
