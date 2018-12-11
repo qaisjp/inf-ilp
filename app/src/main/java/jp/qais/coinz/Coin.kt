b02faf450a57a55ad612e2ce6791e98087e563ae
Qais Patankar <qaisjp@gmail.com>
2018-12-11 00:06:29 +0000

    Add Parcelable implementations to Coin,Currency

--- app/src/main/java/jp/qais/coinz/Coin.kt
+++ app/src/main/java/jp/qais/coinz/Coin.kt
@@ -1,13 +1,35 @@
 package jp.qais.coinz
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.mapbox.mapboxsdk.geometry.LatLng
 
-data class Coin(val id: String, val currency: Currency, val latLng: LatLng, val value: Float) {
+data class Coin(val id: String, val currency: Currency, val latLng: LatLng, val value: Float) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readParcelable(Currency::class.java.classLoader),
+            parcel.readParcelable(LatLng::class.java.classLoader),
+            parcel.readFloat())
+
     fun toMap(): Map<String, Any> = HashMap<String, Any>().apply {
         this["id"] = id
         this["currency"] = currency.toString()
         this["lat"] = latLng.latitude
         this["lng"] = latLng.longitude
         this["value"] = value
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeParcelable(currency, flags)
+        parcel.writeParcelable(latLng, flags)
+        parcel.writeFloat(value)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Coin> {
+        override fun createFromParcel(parcel: Parcel) = Coin(parcel)
+        override fun newArray(size: Int): Array<Coin?> = arrayOfNulls(size)
     }
 }
--- app/src/main/java/jp/qais/coinz/Currency.kt
+++ app/src/main/java/jp/qais/coinz/Currency.kt
@@ -1,8 +1,10 @@
 package jp.qais.coinz
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 
-enum class Currency(val textID: Int, val colorID: Int) {
+enum class Currency(val textID: Int, val colorID: Int) : Parcelable {
     GOLD(R.string.currency_gold, R.color.currency_gold),
     PENY(R.string.currency_peny, R.color.currency_peny),
     DOLR(R.string.currency_dolr, R.color.currency_dolr),
@@ -11,4 +13,12 @@ enum class Currency(val textID: Int, val colorID: Int) {
 
     fun getString(ctx: Context) = ctx.getString(textID)
     fun getColor(ctx: Context) = ctx.getColor(colorID)
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) = parcel.writeString(toString())
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<Currency> {
+        override fun createFromParcel(parcel: Parcel) = Currency.valueOf(parcel.readString())
+        override fun newArray(size: Int): Array<Currency?> = arrayOfNulls(size)
+    }
 }
